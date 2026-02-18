b15e7616cb07ae4243c775350a98a1005d865f26
KravchukOleksandr <122902626+KravchukOleksandr@users.noreply.github.com>
2026-02-18 19:44:19 +0200

    Delete mig10.py

--- mig10.py
+++
@@ -1,9 +0,0 @@
-poly2 = self.zone[self.cur_group][self.cur_poly_idx]                  # [[x,y], ...] (OPEN)
-arr = self.plane_indices[self.cur_group][self.cur_poly_idx]          # [p0,p1,...]  (OPEN)
-
-for i, (x, y) in enumerate(poly2):
-    p = arr[i] if i < len(arr) else -1
-    X, Y = self.viewer.norm_to_canvas(x, y)
-    fill = UNBOUND_COLOR if p == -1 else self._color_plane(str(int(p)))
-    outline = "white" if self.selected_vertex == i else "black"
-    self._circle(X, Y, 8, fill=fill, outline=outline)
